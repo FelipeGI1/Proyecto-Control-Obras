c0ffdec75497bbd2f95a3750ad37acb0af8e2a00
FelipeGI1 <felipe.guerra2101@alumnos.ubiobio.cl>
2026-02-12 14:00:11 -0300

    definicion de la entidad de usuario

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column({ type: 'text', nullable: false})
+    fullname: string;
+
+    @Column({ type: 'text', unique: true, nullable: false})
+    email: string;
+
+    @Column({ type: 'text', nullable: false})
+    password: string;
+
+    @Column({ type: 'text', default: 'visualizador'})
+    role: string;
+
+    @Column({ default: true})
+    isActive: boolean;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
